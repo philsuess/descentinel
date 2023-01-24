0311bd3f62b2eeb53f1fefa9029a27e01015e75e
Phil Süss <philsuess@yahoo.com>
2023-01-24 16:51:52 +0100

    feat(detect_card): :sparkles: add keyword search for card text

--- app/detect_card/detect_card.py
+++ app/detect_card/detect_card.py
@@ -1,5 +1,7 @@
 import cv2
 import pytesseract
+import json
+from typing import Dict
 
 
 def image_as_grayscale(cv2_image):
@@ -23,3 +25,38 @@ def extract_card_text(cv2_image) -> str:
     # cv2.imwrite(f"{image}_denoised.jpg", denoised)
     custom_config = r""
     return pytesseract.image_to_string(cv2_image, config=custom_config)
+
+
+class OverlordCardsKeywordsMatcher:
+    def __init__(self, card_keywords: Dict) -> None:
+        self.overlord_cards = card_keywords
+
+    @classmethod
+    def from_file(cls, file_name: str):
+        with open(file_name) as f:
+            overlord_cards_json = json.load(f)
+            return cls(overlord_cards_json["cards"])
+
+    def search_card_in_database(self, card_text: str) -> Dict:
+        def match_card_text_with_keywords(card_keywords: list[str]) -> int:
+            keyword_matches = 0
+            for keyword in card_keywords:
+                if keyword in card_text:
+                    # print(f"\t\tfound {keyword} in text")
+                    keyword_matches = keyword_matches + 1
+            return keyword_matches
+
+        best_matching_card = {}
+        highest_keyword_match = 0
+        for card in self.overlord_cards:
+            card_score = match_card_text_with_keywords(card["keywords"])
+            # print(f"\t{card['id']} has score {card_score}")
+            if card_score > highest_keyword_match:
+                best_matching_card = card
+                highest_keyword_match = card_score
+
+        return best_matching_card
+
+    def identify(self, cv2_image) -> Dict:
+        text_from_card = extract_card_text(cv2_image=cv2_image)
+        return self.search_card_in_database(text_from_card)
--- app/detect_card/tests/test_detect_card.py
+++ app/detect_card/tests/test_detect_card.py
@@ -1,4 +1,4 @@
-from detect_card import extract_card_text
+from detect_card import extract_card_text, OverlordCardsKeywordsMatcher
 import cv2
 
 
@@ -20,3 +20,32 @@ def test_extract_card_test():
 
     image = cv2.imread("tests/RuneExplosive.jpg_detected.jpg")
     assert "coffre" in extract_card_text(image)
+
+
+def tests_cards_keywords():
+    matcher = OverlordCardsKeywordsMatcher.from_file("./keywords_cards.json")
+    assert (
+        "Dark Balm"
+        == matcher.search_card_in_database("les pions Baum Sombre pions effet")["id"]
+    )
+
+
+def test_identify_cards():
+    matcher = OverlordCardsKeywordsMatcher.from_file("./keywords_cards.json")
+    image = cv2.imread("tests/BaumeSombre_02.jpg_detected.jpg")
+    assert "Dark Balm" == matcher.identify(image)["id"]
+
+    image = cv2.imread("tests/BaumSombre.jpg_detected.jpg")
+    assert "Dark Balm" == matcher.identify(image)["id"]
+
+    image = cv2.imread("tests/ExpodierendeRune.jpg_detected.jpg")
+    assert "Explosive Rune" == matcher.identify(image)["id"]
+
+    image = cv2.imread("tests/Ferrox.jpg_detected.jpg")
+    assert "Ferrox Tribe" == matcher.identify(image)["id"]
+
+    image = cv2.imread("tests/Höllenhunde.jpg_detected.jpg")
+    assert "Hell Hound Pack" == matcher.identify(image)["id"]
+
+    image = cv2.imread("tests/RuneExplosive.jpg_detected.jpg")
+    assert "Explosive Rune" == matcher.identify(image)["id"]
