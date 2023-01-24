0866e6b7a8d182780819e3d7719a20cc941cc19a
Phil Süss <philsuess@yahoo.com>
2023-01-24 16:25:25 +0100

    test(detect_card): :white_check_mark: add more tests for text extraction

--- app/detect_card/tests/test_detect_card.py
+++ app/detect_card/tests/test_detect_card.py
@@ -3,5 +3,20 @@ import cv2
 
 
 def test_extract_card_test():
+    image = cv2.imread("tests/BaumeSombre_02.jpg_detected.jpg")
+    assert "Baume" in extract_card_text(image)
+
+    image = cv2.imread("tests/BaumSombre.jpg_detected.jpg")
+    assert "tous les pions effet" in extract_card_text(image)
+
+    image = cv2.imread("tests/ExpodierendeRune.jpg_detected.jpg")
+    assert "Kune" in extract_card_text(image)
+
     image = cv2.imread("tests/Ferrox.jpg_detected.jpg")
     assert "Ferrox" in extract_card_text(image)
+
+    image = cv2.imread("tests/Höllenhunde.jpg_detected.jpg")
+    assert "hund" in extract_card_text(image)
+
+    image = cv2.imread("tests/RuneExplosive.jpg_detected.jpg")
+    assert "coffre" in extract_card_text(image)
