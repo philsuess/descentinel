424de5543bcd103afea83ea69f903b2f135c87ef
philsuess <philsuess@users.noreply.github.com>
2023-04-23 22:05:43 +0200

    fix(app): make sure script runs without errors

--- app/setup_frontend.sh
+++ app/setup_frontend.sh
@@ -3,5 +3,6 @@ cd view
 podman build --rm -t frontend_builder .
 podman run --rm -v ./:/app frontend_builder
 cd ..
+touch /var/www/html/dummy
 rm -R /var/www/html/*
 cp -r view/dist/* /var/www/html/
