57be96dc2a2bd83514b4362dc8e1a7ce5fdc8bc8
philsuess <philsuess@users.noreply.github.com>
2023-04-23 22:05:43 +0200

    chore(app): remove build images to free up space

--- app/build.sh
+++ app/build.sh
@@ -1,12 +1,17 @@
 cd broadcast
 sh ./build.sh
+podman image rm localhost/broadcast_builder
 cd ..
 
 cd monitor
 sh ./build.sh
+podman image rm localhost/monitor_builder
 cd ..
 cp monitor/target/release/monitor ./monitor_app
 
+podman image rm docker.io/library/rust
+
 cd detect_card
 sh ./build.sh
+podman image rm docker.io/library/python
 cd ..
