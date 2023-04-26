e807751d06bddbb15f1c5fc0fdd3ca9de1c8d1b1
Phil Süss <philsuess@yahoo.com>
2023-04-26 16:35:27 +0200

    chore(app): :wrench: don't remove images

--- app/setup_frontend.sh
+++ app/setup_frontend.sh
@@ -4,8 +4,8 @@ apt install nginx
 cd view
 podman build --rm -t frontend_builder .
 podman run --rm -v ./:/app frontend_builder
-podman image rm docker.io/library/node
-podman image rm localhost/frontend_builder
+#podman image rm docker.io/library/node
+#podman image rm localhost/frontend_builder
 cd ..
 touch /var/www/html/dummy
 rm -R /var/www/html/*
