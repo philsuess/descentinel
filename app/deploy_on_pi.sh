95949a9964d51dd9bba900813583090529fab6a5
Phil Süss <philsuess@yahoo.com>
2023-06-07 19:00:53 +0200

    build(app): :wrench: update all scripts

--- app/deploy_on_pi.sh
+++ app/deploy_on_pi.sh
@@ -17,4 +17,9 @@ cd monitor
 scp ./target/aarch64-unknown-linux-gnu/release/monitor ${TARGET_HOST}:${TARGET_PATH}
 cd ..
 
+cd detect_card
+scp ./target/aarch64-unknown-linux-gnu/release/detect_card ${TARGET_HOST}:${TARGET_PATH}
+scp ./Containerfile.pi ${TARGET_HOST}:${TARGET_PATH}/Containerfile.detect_card.service
+cd ..
+
 scp ./run_on_pi.sh ${TARGET_HOST}:${TARGET_PATH}/..
