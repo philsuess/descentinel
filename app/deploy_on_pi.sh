6de69b731073aca30aa35ca3cd75a37a76cf3faf
Phil Süss <philsuess@yahoo.com>
2023-06-25 16:16:29 +0200

    fix(app): :ambulance: some corrections re. pi deployment

--- app/deploy_on_pi.sh
+++ app/deploy_on_pi.sh
@@ -18,7 +18,8 @@ scp ./target/aarch64-unknown-linux-gnu/release/monitor ${TARGET_HOST}:${TARGET_P
 cd ..
 
 cd detect_card
-scp ./target/aarch64-unknown-linux-gnu/release/detect_card ${TARGET_HOST}:${TARGET_PATH}
+scp ./target/aarch64-unknown-linux-gnu/release/detect_card ${TARGET_HOST}:${TARGET_PATH}/release
+scp ./keywords_cards.json ${TARGET_HOST}:${TARGET_PATH}
 scp ./Containerfile.pi ${TARGET_HOST}:${TARGET_PATH}/Containerfile.detect_card.service
 cd ..
 
