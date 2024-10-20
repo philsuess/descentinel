3a197864ac60f2f79c8be7b34d17176848cdfa2b
Phil Suess <philsuess@yahoo.com>
2024-10-20 14:39:05 +0200

    refactor(detect_card): :recycle: split to lib and main
    and adopt new ipc abstraction

--- app/detect_card/src/main.rs
+++ app/detect_card/src/main.rs
@@ -1,11 +1,9 @@
 use clap::Parser;
-use descentinel_types::ipc::{self, Ipc, RabbitMqiIpc};
-use futures::StreamExt;
-use lapin::{options::*, types::FieldTable, BasicProperties, Channel, Connection, Consumer};
+use descentinel_types::ipc::{self, IpcError, Message};
+use detect_card::{identify_card, load_overlord_keywords, OverlordCards};
+use lapin::Connection;
 use log::{error, info};
-use serde::Deserialize;
-use std::time::Duration;
-use tesseract::Tesseract;
+use std::{sync::Arc, time::Duration};
 use thiserror::Error;
 use tokio::join;
 
@@ -42,25 +40,25 @@ async fn main() -> Result<(), DetectCardError> {
     info!("DETECT_CARD service starting");
     let args = Args::parse();
 
-    let overlord_cards = load_overlord_keywords(&args.overlord_cards_keywords_file);
-    let connection = RabbitMqiIpc::establish_connection(&args.ampq_url).await?;
+    let overlord_cards = Arc::new(load_overlord_keywords(&args.overlord_cards_keywords_file));
+    let connection = ipc::create_connection(&args.ampq_url).await?;
     info!("Established connection to {}", args.ampq_url);
 
-    let _ = join!(rabbitmq_listen(&connection, &args, &overlord_cards));
+    let _ = join!(rabbitmq_listen(connection, &args, overlord_cards));
 
     Ok(())
 }
 
 async fn rabbitmq_listen(
-    connection: &Connection,
+    connection: Arc<Connection>,
     args: &Args,
-    overlord_cards: &OverlordCards,
+    overlord_cards: Arc<OverlordCards>,
 ) -> Result<(), DetectCardError> {
     let mut retry_interval = tokio::time::interval(Duration::from_secs(5));
     loop {
         retry_interval.tick().await;
         info!("connecting rabbitmq consumer...");
-        match init_rabbitmq_listen(connection, args, overlord_cards).await {
+        match init_rabbitmq_listen(connection.clone(), args, overlord_cards.clone()).await {
             Ok(_) => info!("connection to rabbitmq established"),
             Err(e) => error!(
                 "error when trying to establish connection to rabbitmq: {}",
@@ -70,217 +68,54 @@ async fn rabbitmq_listen(
     }
 }
 
-async fn init_rabbitmq_listen(
-    connection: &Connection,
-    args: &Args,
-    overlord_cards: &OverlordCards,
-) -> Result<(), DetectCardError> {
-    let channel_images =
-        RabbitMqiIpc::create_channel(connection, &args.game_room_feed_queue).await?;
+async fn init_queues(connection: Arc<Connection>, args: &Args) -> Result<(), IpcError> {
+    let _results = tokio::join!(
+        ipc::declare_queue(connection.clone(), &args.game_room_feed_queue),
+        ipc::declare_queue(connection.clone(), &args.detected_ol_cards_queue),
+        ipc::declare_queue(connection.clone(), &args.short_log_queue)
+    );
     info!(
         "Awaiting game room images from {}",
         args.game_room_feed_queue
     );
-    let game_room_images_consumer = channel_images
-        .basic_consume(
-            &args.game_room_feed_queue,
-            "",
-            BasicConsumeOptions::default(),
-            FieldTable::default(),
-        )
-        .await?;
-
-    let channel_detected_ol_cards =
-        RabbitMqiIpc::create_channel(connection, &args.detected_ol_cards_queue).await?;
     info!(
         "Sending detected OL cards to {}",
         args.detected_ol_cards_queue
     );
-
-    let channel_short_logs =
-        RabbitMqiIpc::create_channel(connection, &args.short_log_queue).await?;
     info!("Logging to {}", args.short_log_queue);
-
-    let consume_future = consume_game_room_feed(
-        game_room_images_consumer,
-        overlord_cards,
-        &channel_detected_ol_cards,
-        &channel_short_logs,
-        args,
-    );
-
-    futures::join!(consume_future);
-
     Ok(())
 }
 
-async fn consume_game_room_feed(
-    mut game_room_feed: Consumer,
-    overlord_cards: &OverlordCards,
-    channel_detected_ol_cards: &Channel,
-    channel_logs: &Channel,
+async fn init_rabbitmq_listen(
+    connection: Arc<Connection>,
     args: &Args,
-) {
-    while let Some(delivery) = game_room_feed.next().await {
-        let delivery = delivery.expect("error in consumer");
-        delivery.ack(BasicAckOptions::default()).await.expect("ack");
-        //  info!("received {:?}", delivery);
-        if let Some(card_id) = identify_card(&delivery.data, overlord_cards) {
-            let _ = send_over_queue(
-                card_id.as_bytes(),
-                channel_detected_ol_cards,
-                &args.detected_ol_cards_queue,
-            )
-            .await;
-            let mut log_message = String::from("detected OL card ");
-            log_message.push_str(&card_id);
-            info!("{}", &log_message);
-            let _ =
-                send_over_queue(log_message.as_bytes(), channel_logs, &args.short_log_queue).await;
-        }
-    }
-}
-
-async fn send_over_queue(
-    payload: &[u8],
-    channel: &Channel,
-    queue_name: &str,
+    overlord_cards: Arc<OverlordCards>,
 ) -> Result<(), DetectCardError> {
-    channel
-        .basic_publish(
-            "",
-            queue_name,
-            BasicPublishOptions::default(),
-            payload,
-            BasicProperties::default(),
-        )
-        .await?
-        .await?;
+    init_queues(connection.clone(), args).await?;
+
+    let detect_ol_card = move |game_room_image: &Message| {
+        handle_game_room_image(game_room_image, overlord_cards.clone())
+    };
+
+    let _results = tokio::join!(ipc::process_message_pipeline(
+        connection.clone(),
+        &args.game_room_feed_queue,
+        &args.detected_ol_cards_queue,
+        detect_ol_card,
+    ));
+
     Ok(())
 }
 
-#[derive(Deserialize, Debug)]
-struct OverlordCards {
-    cards: Vec<CardKeywords>,
-}
-
-impl OverlordCards {
-    fn id_of_best_keywords_match(&self, card_text: &str) -> Option<String> {
-        let winning_card = self
-            .cards
-            .iter()
-            .reduce(|max_found, candidate| {
-                if candidate.number_of_matches(card_text) > max_found.number_of_matches(card_text) {
-                    candidate
-                } else {
-                    max_found
-                }
-            })
-            .unwrap();
-        if winning_card.number_of_matches(card_text) == 0 {
-            None
-        } else {
-            Some(winning_card.id.clone())
-        }
-    }
-}
-
-#[derive(Deserialize, Debug)]
-struct CardKeywords {
-    id: String,
-    keywords: Vec<String>,
-}
-
-impl CardKeywords {
-    fn number_of_matches(&self, card_text: &str) -> u8 {
-        self.keywords.iter().fold(0, |sum_matches, next_keyword| {
-            if card_text.contains(next_keyword.as_str()) {
-                return sum_matches + 1;
-            }
-            sum_matches
-        })
-    }
-}
-
-fn identify_card(card_image_buffer: &[u8], overlord_cards: &OverlordCards) -> Option<String> {
-    let card_text = extract_card_text_from_buffer(card_image_buffer, "fra");
-    overlord_cards.id_of_best_keywords_match(&card_text)
-}
-
-fn load_overlord_keywords(file_name: &str) -> OverlordCards {
-    let file = std::fs::File::open(file_name).unwrap();
-    let reader = std::io::BufReader::new(file);
-    serde_json::from_reader(reader).expect("file is not proper json")
-}
-
-fn extract_card_text_from_buffer(card_image_buffer: &[u8], language: &str) -> String {
-    match Tesseract::new(None, Some(language))
-        .unwrap()
-        .set_image_from_mem(card_image_buffer)
-        .unwrap()
-        .recognize()
-        .unwrap()
-        .get_text()
-    {
-        Ok(card_text) => card_text,
-        Err(_) => String::from("could not read card text"),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use image::{ImageBuffer, ImageOutputFormat, Rgb};
-    use std::io::Cursor;
-
-    fn convert_to_bytes_buffer(image: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Vec<u8> {
-        let mut bytes = Vec::new();
-        image
-            .write_to(&mut Cursor::new(&mut bytes), ImageOutputFormat::Png)
-            .unwrap();
-        bytes
+fn handle_game_room_image(
+    game_room_image: &Message,
+    overlord_cards: Arc<OverlordCards>,
+) -> Option<Message> {
+    if let Some(card_id) = identify_card(&game_room_image.content, overlord_cards.as_ref()) {
+        return Some(Message {
+            content: card_id.as_bytes().to_vec(),
+        });
     }
 
-    #[test]
-    fn overlord_cards_detection_from_file_works() {
-        let card_text_baume_sombre =
-            tesseract::ocr("test_images/BaumeSombre_02.jpg_detected.jpg", "fra").unwrap();
-        println!("{}", card_text_baume_sombre);
-        assert!(card_text_baume_sombre.contains("Baume"));
-
-        let card_text_explodierende_rune =
-            tesseract::ocr("test_images/ExplodierendeRune.jpg_detected.jpg", "fra").unwrap();
-        println!("{}", card_text_explodierende_rune);
-        assert!(card_text_explodierende_rune.contains("Schatztruhe"));
-    }
-
-    #[test]
-    fn overlord_cards_detection_from_memory_works() {
-        let card_image = image::open("test_images/BaumeSombre_02.jpg_detected.jpg")
-            .unwrap()
-            .to_rgb8();
-
-        let card_text = extract_card_text_from_buffer(&convert_to_bytes_buffer(&card_image), "fra");
-        println!("{}", card_text);
-        assert!(card_text.contains("Baume"));
-    }
-
-    #[test]
-    fn keywords_cards_json_file_is_ok() {
-        let overlord_cards = load_overlord_keywords("keywords_cards.json");
-        assert!(overlord_cards.cards.len() > 3);
-    }
-
-    #[test]
-    fn known_overlord_cards_detection_works() {
-        let overlord_cards = load_overlord_keywords("keywords_cards.json");
-
-        let card_image = image::open("test_images/BaumeSombre_02.jpg_detected.jpg")
-            .unwrap()
-            .to_rgb8();
-        assert_eq!(
-            Some(String::from("dark_balm")),
-            identify_card(&convert_to_bytes_buffer(&card_image), &overlord_cards)
-        );
-    }
+    None
 }
