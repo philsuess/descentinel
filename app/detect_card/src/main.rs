8e7c6cb5000fc67bb17d585742e92754326d95c0
Phil Suess <philsuess@yahoo.com>
2024-11-10 08:18:33 +0100

    refactor(detect_card): :recycle: assume cards are tagged with qr from now on
    this is a breaking change for the entire app

--- app/detect_card/src/main.rs
+++ app/detect_card/src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use descentinel_types::ipc::{self, IpcError, Message};
-use detect_card::{identify_card, load_overlord_keywords, OverlordCards};
+use detect_card::{convert_to_grey_image, identify_card_from};
 use lapin::Connection;
 use log::{error, info};
 use std::{sync::Arc, time::Duration};
@@ -18,7 +18,7 @@ pub enum DetectCardError {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = String::from("Q_CARD_IMAGE"))]
+    #[arg(short, long, default_value_t = String::from("Q_GAME_ROOM_FEED"))]
     game_room_feed_queue: String,
 
     #[arg(short, long, default_value_t = String::from("Q_DETECTED_OL_CARDS"))]
@@ -29,9 +29,6 @@ struct Args {
 
     #[arg(short, long, default_value_t = String::from("amqp://localhost:5672"))]
     ampq_url: String,
-
-    #[arg(short, long, default_value_t = String::from("keywords_cards.json"))]
-    overlord_cards_keywords_file: String,
 }
 
 #[tokio::main]
@@ -40,11 +37,10 @@ async fn main() -> Result<(), DetectCardError> {
     info!("DETECT_CARD service starting");
     let args = Arc::new(Args::parse());
 
-    let overlord_cards = Arc::new(load_overlord_keywords(&args.overlord_cards_keywords_file));
     let connection = ipc::create_connection(&args.ampq_url).await?;
     info!("Established connection to {}", args.ampq_url);
 
-    let _ = join!(rabbitmq_listen(connection, args, overlord_cards));
+    let _ = join!(rabbitmq_listen(connection, args));
 
     Ok(())
 }
@@ -52,13 +48,12 @@ async fn main() -> Result<(), DetectCardError> {
 async fn rabbitmq_listen(
     connection: Arc<Connection>,
     args: Arc<Args>,
-    overlord_cards: Arc<OverlordCards>,
 ) -> Result<(), DetectCardError> {
     let mut retry_interval = tokio::time::interval(Duration::from_secs(5));
     loop {
         retry_interval.tick().await;
         info!("connecting rabbitmq consumer...");
-        match init_rabbitmq_listen(connection.clone(), args.clone(), overlord_cards.clone()).await {
+        match init_rabbitmq_listen(connection.clone(), args.clone()).await {
             Ok(_) => info!("connection to rabbitmq established"),
             Err(e) => error!(
                 "error when trying to establish connection to rabbitmq: {}",
@@ -89,14 +84,12 @@ async fn init_queues(connection: Arc<Connection>, args: Arc<Args>) -> Result<(),
 async fn init_rabbitmq_listen(
     connection: Arc<Connection>,
     args: Arc<Args>,
-    overlord_cards: Arc<OverlordCards>,
 ) -> Result<(), DetectCardError> {
     let args_clone = args.clone();
     init_queues(connection.clone(), args.clone()).await?;
 
-    let detect_ol_card = move |game_room_image: &Message| {
-        handle_game_room_image(game_room_image, overlord_cards.clone(), args.clone())
-    };
+    let detect_ol_card =
+        move |game_room_image: &Message| handle_game_room_image(game_room_image, args.clone());
 
     let game_room_feed_queue = &args_clone.game_room_feed_queue;
     let _results = tokio::join!(ipc::process_message_pipeline(
@@ -108,13 +101,10 @@ async fn init_rabbitmq_listen(
     Ok(())
 }
 
-fn handle_game_room_image(
-    game_room_image: &Message,
-    overlord_cards: Arc<OverlordCards>,
-    args: Arc<Args>,
-) -> Vec<(String, Message)> {
+fn handle_game_room_image(game_room_image: &Message, args: Arc<Args>) -> Vec<(String, Message)> {
     let mut downstream_messages = Vec::new();
-    if let Some(card_id) = identify_card(&game_room_image.content, overlord_cards.as_ref()) {
+    let game_room_in_grey = convert_to_grey_image(&game_room_image.content);
+    if let Some(card_id) = identify_card_from(&game_room_in_grey) {
         downstream_messages.push((
             args.detected_ol_cards_queue.clone(),
             Message {
