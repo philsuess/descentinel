48a8b4b2bf6bfdba4067c20069d062ea96eb35a7
Phil Suess <philsuess@yahoo.com>
2024-11-27 18:47:15 +0100

    feat(presentation): :sparkles: manually redraw game room image

--- app/presentation/src/main.rs
+++ app/presentation/src/main.rs
@@ -1,10 +1,34 @@
 use leptos::*;
+use rand::Rng;
 use thaw::*;
+
+// thaw components at https://thaw-85fsrigp0-thaw.vercel.app/components/button
+
+#[component]
+fn GameRoomImage(
+    /// Image source
+    #[prop(into)]
+    src: ReadSignal<String>,
+) -> impl IntoView {
+    view! {
+        <Image src=src />
+    }
+}
 
 #[component]
 fn App() -> impl IntoView {
+    let mut rng = rand::thread_rng();
+    let (text, set_text) = create_signal(String::from("http://0.0.0.0:3030/Q_GAME_ROOM_FEED"));
+
     view! {
-        <Image src="http://0.0.0.0:3030/Q_GAME_ROOM_FEED"/>
+        <button
+            on:click=move |_| {
+                set_text.set([String::from("http://0.0.0.0:3030/Q_GAME_ROOM_FEED"),rng.gen::<u8>().to_string()].join("?"));
+            }
+        >
+            "Refresh image"
+        </button>
+        <GameRoomImage src=text />
     }
 }
 
