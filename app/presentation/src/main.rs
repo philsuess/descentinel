2e896e31e1f65143da21d303d743cf26b5c52d4e
Phil Suess <philsuess@yahoo.com>
2024-12-07 15:50:43 +0100

    add log viewer component

--- app/presentation/src/main.rs
+++ app/presentation/src/main.rs
@@ -5,6 +5,49 @@ use thaw::*;
 // thaw components at https://thaw-85fsrigp0-thaw.vercel.app/components/button
 
 #[component]
+fn LogViewer(
+    /// Log source
+    #[prop(into)]
+    url: String,
+) -> impl IntoView {
+    let mut cumulated_log = String::from("");
+    let (log_message, set_log_message) = create_signal(String::from("no log messages yet"));
+
+    let resource = create_resource(
+        || (),
+        move |_| {
+            let url = url.clone(); // Clone the URL for use inside the async block
+            async move {
+                // Perform a GET request to the provided URL
+                match reqwest::get(&url).await {
+                    Ok(response) => {
+                        if response.status().is_success() {
+                            response
+                                .text()
+                                .await
+                                .unwrap_or_else(|_| "Failed to read response".into())
+                        } else {
+                            format!("Request failed with status: {}", response.status())
+                        }
+                    }
+                    Err(err) => format!("Failed to make request: {}", err),
+                }
+            }
+        },
+    );
+
+    create_effect(move |_| {
+        if let Some(latest_log) = resource.get() {
+            let cumulated_log = [cumulated_log.clone(), latest_log.clone()].join("\n");
+            set_log_message.set(cumulated_log.clone());
+            resource.refetch();
+        }
+    });
+
+    view! {<p>{move || log_message.get()}</p>}
+}
+
+#[component]
 fn GameRoomImage(
     /// Image source
     #[prop(into)]
@@ -35,10 +78,9 @@ fn GameRoomImage(
 
 #[component]
 fn App() -> impl IntoView {
-    let game_room_feed_url = String::from("http://0.0.0.0:3030/Q_GAME_ROOM_FEED");
-
     view! {
-        //<GameRoomImage src=game_room_feed_url />
+        <LogViewer url=String::from("http://0.0.0.0.:3030/Q_SHORT_LOG") />
+        <GameRoomImage src=String::from("http://0.0.0.0:3030/Q_GAME_ROOM_FEED") />
     }
 }
 
