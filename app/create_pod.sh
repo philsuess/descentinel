783f9edbd276d4eaa1865ae1b3aca497f55d6809
Phil Suess <philsuess@yahoo.com>
2025-01-12 17:47:55 +0100

    fix(services): fix image names in script

--- app/create_pod.sh
+++ app/create_pod.sh
@@ -4,5 +4,5 @@ podman pod rm -f descentinel
 
 podman pod create -p 15672:15672 -p 5672:5672 -p 3030:3030 --name descentinel
 podman run -d --pod descentinel --restart=always --name rabbitmq docker.io/rabbitmq:4-management
-podman run -d --pod descentinel --restart=always --name detect_card -e RUST_LOG=info -e RABBITMQ_AMQP_URL=0.0.0.0 detect_card
-podman run -d --pod descentinel --restart=always --name broadcast -e RUST_LOG=info broadcast
+podman run -d --pod descentinel --restart=always --name detect_card -e RUST_LOG=info -e RABBITMQ_AMQP_URL=0.0.0.0 detect_card_service
+podman run -d --pod descentinel --restart=always --name broadcast -e RUST_LOG=info broadcast_service
